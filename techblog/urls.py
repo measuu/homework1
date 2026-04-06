5e5a3391d971e19a63ac29115e2a17a6165ea152
measuu <orlyk.do@gmail.com>
2026-04-06 14:47:18 +0200

    Delete techblog directory

--- techblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('about_view/', views.about_view, name="about_us"),
-    path('contact/', views.contact_view, name="contact"),
-]
-
